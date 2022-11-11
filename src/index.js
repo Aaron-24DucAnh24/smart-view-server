14fba12479742824576a2885b7899ee94a443f33
Aaron-24DucAnh24 <90702045+Aaron-24DucAnh24@users.noreply.github.com>
2022-11-11 21:46:18 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// compulsory library
+const express = require('express')
+
+const app = express()
+var server = require("http").createServer(app);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('This is the web!')
+})
+
+server.listen(port)
+console.log('--> Run successfully')
