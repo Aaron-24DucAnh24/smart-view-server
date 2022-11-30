3fd6a7c29adcd7ab9efc0f45d99279ebc5c37199
Aaron-24DucAnh24 <90702045+Aaron-24DucAnh24@users.noreply.github.com>
2022-11-30 23:49:01 +0700

    Update authen.js

--- src/middleware/authen.js
+++ src/middleware/authen.js
@@ -1,11 +1,7 @@
 
 function checker(req, res, next) {
-    if(req.session.user){
-        next()
-    }
-    else{
-        res.redirect('/login')
-    }
+    if(req.session.user) next()
+    else res.redirect('/login')
 }
 
 module.exports = checker
