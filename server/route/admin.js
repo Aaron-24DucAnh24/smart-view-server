c07af4126569ac96df0fef1c0fda4287ab554517
Aaron-24DucAnh24 <90702045+Aaron-24DucAnh24@users.noreply.github.com>
2023-01-05 11:24:28 +0700

    Update admin.js

--- server/route/admin.js
+++ server/route/admin.js
@@ -4,8 +4,8 @@ const adminController = require('../controller/admin')
 const middleware = require('../middleware')
 
 router.get('/preview', middleware.isAdmin, adminController.preview)
-router.get('/:postID', adminController.getPost)
-router.delete('/:postID', adminController.deletePost)
-router.get('/approve/:postIP', adminController.approvePost)
+router.get('/:postID', middleware.isAdmin, adminController.getPost)
+router.delete('/:postID', middleware.isAdmin, adminController.deletePost)
+router.get('/approve', middleware.isAdmin, adminController.approvePost)
 
 module.exports = router
